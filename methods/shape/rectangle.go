2a36105ce6d3c1b7154af8912fc1c50cf398d207
Edijs-Elijs-Skroders <edijsskroders@gmail.com>
2025-06-11 13:52:52 +0300

    shape

---
+++ methods/shape/rectangle.go
@@ -0,0 +1,6 @@
+package shape
+
+//TODO: create a Rectangle struct, that has a field Height of type float64
+//  and a field Width of type float64
+
+//Implement Shape interface methods
---
+++ methods/shape/shape.go
@@ -0,0 +1,15 @@
+package shape
+
+//TODO: add method Area() to the interface, which returns float64 
+// when implementing the interface, this should return the area of the shape
+//TODO: add method Parameter() to the interface, which returns float64 
+//when implementing interface, this should return the parameter of the shape
+//TODO: add method Resize() to the interface, which takes type float64 paramter named "resizer"
+//when implementing interface, this should resize the shapes values, e.g. shape.radius = shape.radius * resizer
+type Shape interface{
+}
+
+
+//TODO: implement a function ResizeAll, that has a parameter of type Shape slice,
+//and a parameter resizer of type float64
+//This function should call Resize() on all of the shapes in the slice
