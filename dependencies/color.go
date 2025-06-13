c56d0d25efc8e95b20833d5c19daf543b69de49c
Edijs-Elijs-Skroders <edijsskroders@gmail.com>
2025-06-13 14:36:30 +0300

    concurrency

---
+++ dependencies/color.go
@@ -0,0 +1,8 @@
+package dependencies
+
+// import "github.com/fatih/color"
+//TODO: Download
+//TODO: Use the external package to print out the s string in BgHiRed color
+func ColorPrint(s string) {
+
+}
